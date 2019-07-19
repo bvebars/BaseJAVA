c0fbdac98ec156f4c7f74460e4c4c0daf28de21a
bvebars <37873824+bvebars@users.noreply.github.com>
2019-07-19 17:38:57 +0700

    Update ArrayStorage.java
    Заполнение методов:

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,44 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = r;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            System.out.println("Первое: " + uuid);
+            if(storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
+//        return storage[0];
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if(storage[i].uuid.equals(uuid)) {
+              storage[i] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, storage.length);
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
