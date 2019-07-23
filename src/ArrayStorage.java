0fc61c4a04a16cdfe88816dab0226f95af12025a
topjava <bvebars@gmail.com>
2019-07-23 19:11:28 +0700

    Переработка всех методов.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    Resume[] saveUuid = new Resume[1];
 
     void clear() {
         for (int i = 0; i < storage.length; i++) {
@@ -13,9 +14,8 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            storage[i] = r;
-        }
+        storage[saveUuid.length - 1] = r;
+        saveUuid = Arrays.copyOf(storage,saveUuid.length + 1);
     }
 
     Resume get(String uuid) {
@@ -29,19 +29,52 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
+        Resume[] renameUuid = new Resume[size()];
+        int incUuid = 0;
+        //Поиск и изменение ячейки на null
         for (int i = 0; i < storage.length; i++) {
-            if (storage[i].uuid.equals(uuid)) storage[i] = null;
+            if (storage[i] != null) {
+
+                if (storage[i].uuid.equals(uuid)) {
+                    storage[i] = null;
+                }
+            }
         }
+        //Создание нового массива без дырок, ограниченной длинны
+        for (int i = 0; i < storage.length; i++) {
+            if(storage[i] != null) {
+                renameUuid[incUuid] = storage[i];
+                incUuid++;
+            }
+        }
+
+        storage = Arrays.copyOf(renameUuid, 10000);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, storage.length);
+
+        Resume [] arr = new Resume[size()];
+        System.out.println( "размер: " + size());
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                Arrays.copyOf(arr, arr.length + 1);
+                arr[i] = storage[i];
+            }
+        }
+        return arr;
     }
 
     int size() {
-       return storage.length;
+        int amountResume = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                amountResume++;
+            }
+        }
+       return amountResume;
     }
 }
