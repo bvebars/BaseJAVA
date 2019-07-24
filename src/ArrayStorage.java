f2c2264b6e5cebdc17a84a0e04590a3707ee6b2d
topjava <bvebars@gmail.com>
2019-07-24 15:33:03 +0700

    Удаление цикла в методе delete, переработка метода save.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,51 +5,40 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int incSave = 1;
-    private int size = size();
+    private int size;
 
     void clear() {
         for (int i = 0; i < size; i++) {
-            if (storage[i] != null) {
-                storage[i] = null;
-            }
+            storage[i] = null;
         }
     }
 
     void save(Resume r) {
-        storage[incSave - 1] = r;
-        storage = Arrays.copyOf(storage, incSave + 1);
-        incSave++;
+        storage[storage.length - 10000] = r;
+        storage = Arrays.copyOf(storage, storage.length + 1);
     }
 
     Resume get(String uuid) {
-        size = size();
         for (int i = 0; i < size; i++) {
-            if (storage[i] != null) {
-                if (storage[i].uuid.equals(uuid)) {
-                    System.out.println(storage[i]);
-                    return storage[i];
-                }
-            }
+            System.out.println(storage[i]);
+            if (storage[i].uuid.equals(uuid)) {
 
+                System.out.println(storage[i]);
+                return storage[i];
+            }
         }
         return null;
     }
 
     void delete(String uuid) {
-        Resume[] renameUuid = new Resume[3];
+        Resume[] renameUuid = new Resume[size];
         int incUuid = 0;
-        //Поиск и изменение ячейки на null
-        for (int i = 0; i < size; i++) {
-            if (storage[i] != null) {
 
-                if (storage[i].uuid.equals(uuid)) {
-                    storage[i] = null;
-                }
-            }
-        }
-        //Создание нового массива без дырок, ограниченной длинны
         for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+            }
+
             if (storage[i] != null) {
                 renameUuid[incUuid] = storage[i];
                 incUuid++;
@@ -66,21 +55,19 @@ public class ArrayStorage {
         size = size();
         Resume[] completedResumes = new Resume[size];
         for (int i = 0; i < size; i++) {
-            if (storage[i] != null) {
-                Arrays.copyOf(completedResumes, completedResumes.length + 1);
-                completedResumes[i] = storage[i];
-            }
+            Arrays.copyOf(completedResumes, completedResumes.length + 1);
+            completedResumes[i] = storage[i];
         }
         return completedResumes;
     }
 
     int size() {
-        int amountResume = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                amountResume++;
+        int size = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                size++;
             }
         }
-        return amountResume;
+        return size;
     }
 }
